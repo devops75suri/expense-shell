b61b36209e978f4ce8ab358fb899da2533664749
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-11 16:12:35 +0530

    execute scripting backend

--- common.sh
+++ common.sh
@@ -4,11 +4,11 @@ download_and_extract() {
 
 echo download the $component code
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-echo $?
+stat_check
 
 echo extracting the file
 unzip /tmp/$component.zip >>$log_file
-echo $?
+stat_check
 }
 
 stat_check() {
