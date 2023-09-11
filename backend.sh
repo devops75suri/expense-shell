47dec720823ae5f935fa77668868621293981efa
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-11 15:53:08 +0530

    execute scripting backend

--- backend.sh
+++ backend.sh
@@ -15,7 +15,10 @@ cp backend.service /etc/systemd/system/backend.service &>>$log_file
 stat_check
 
 echo adding user
-useradd expense &>>$log_file
+id expense &>>$log_file
+if [ $? -ne 0 ]; then
+ useradd expense &>>$log_file
+fi
 stat_check
 
 echo createing directory
