87fd8d37f98bee5eac38c1fc9489fb91c3f726cb
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-14 20:31:53 +0530

    execute scripting backend

--- backend.sh
+++ backend.sh
@@ -9,11 +9,13 @@ useradd expense
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+
 cd /app
+
 unzip /tmp/backend.zip
 
+cd /app
 npm install
-
 
 systemctl daemon-reload
 
@@ -22,5 +24,4 @@ systemctl start backend
 
 dnf install mysql -y
 
-mysql -h 172.31.29.186 -uroot -pExpenseApp@1 < /app/schema/backend.sql
-
+mysql -h 172.31.28.207 -uroot -pExpenseApp@1 < /app/schema/backend.sql
