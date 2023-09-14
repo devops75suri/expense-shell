6ae8ddbd84f017cc1faf5c40ba3790ae751ff9f5
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-14 20:59:49 +0530

    execute scripting backend

--- mysql.sh
+++ mysql.sh
@@ -5,6 +5,7 @@ dnf install mysql-community-server -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 systemctl enable mysqld
+
 systemctl start mysqld
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
