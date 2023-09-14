ad31ad01196a3dfc9f57b43ce5b46bcdbefe3354
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-14 11:22:39 +0530

    execute scripting backend

--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,11 @@
 dnf module disable mysql -y
+
+dnf install mysql-community-server -y
+
+systemctl enable mysqld
+systemctl start mysqld
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+#mysql -uroot -pExpenseApp@1
+
