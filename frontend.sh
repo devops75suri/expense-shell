e0c8a987ed8cbbc953b4514da08de7c75459fed7
Yalaga surendra babu <yalagasurendrababu6@gmail.com>
2023-09-15 14:09:00 +0530

    execute scripting backend

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,20 @@
-dnf install nginx -y
+echo installing the nginx
+dnf install nginx -y >/tmp//expense.log
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo placling expense config file in nginx
+cp expense.conf /etc/nginx/default.d/expense.conf >/tmp//expense.log
 
-systemctl enable nginx
-systemctl start nginx
+echo removeing old content
+rm -rf /usr/share/nginx/html/* >/tmp//expense.log
 
-rm -rf /usr/share/nginx/html/*
+echo download the frontend code
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tmp//expense.log
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html >/tmp//expense.log
 
-cd /usr/share/nginx/html
+echo extracting the forntend code
+unzip /tmp/frontend.zip >/tmp//expense.log
 
-unzip /tmp/frontend.zip
-
-systemctl restart nginx
+echo starting the nginx service
+systemctl enable nginx >/tmp//expense.log
+systemctl restart nginx >/tmp//expense.log
